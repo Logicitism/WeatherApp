979f4983374eeaf46705f9491d903b69bfda3425
Savio Saji Perumpral <24bcab58@kristujayanti.com>
2024-11-15 22:13:54 +0530

    Update homepage.dart

--- homepage.dart
+++ homepage.dart
@@ -35,13 +35,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage("assets/weatherimage.jpg"),
-        fit: BoxFit.fill,
-      )),
-      child: Scaffold(
+    return Scaffold(
+      extendBodyBehindAppBar: true,
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
             title: Padding(
@@ -53,22 +48,29 @@ class _HomePageState extends State<HomePage> {
                     onSubmitted: fetchData,
                     decoration: InputDecoration(
                       hintText: 'Search for a city',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
+                      hintStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(Icons.search,color: Colors.white,),
+                      border:
+                      OutlineInputBorder(
                           borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(32),
-                        topRight: Radius.circular(32),
-                        bottomLeft: Radius.circular(32),
-                        bottomRight: Radius.circular(32),
-                      )),
+                            topLeft: Radius.circular(32),
+                            topRight: Radius.circular(32),
+                            bottomLeft: Radius.circular(32),
+                            bottomRight: Radius.circular(32),
+                          )),
                     ),
                   ),
                 ],
               ),
             ),
-            backgroundColor: Colors.transparent,
+            backgroundColor: Colors.transparent
           ),
           body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/weatherimage.jpg"),
+                  fit: BoxFit.fill,
+                )),
             child: Center(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
@@ -78,7 +80,7 @@ class _HomePageState extends State<HomePage> {
                     height: 360,
                     padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
-                      color: Colors.blueGrey,
+                      color: Color.fromRGBO(225, 225, 225, 100),
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: Column(
@@ -94,7 +96,7 @@ class _HomePageState extends State<HomePage> {
                                   ? "..."
                                   : "${data["current"]["temp_c"]}°c",
                               style:
-                                  TextStyle(fontSize: 48, color: Colors.white),
+                              TextStyle(fontSize: 48, color: Colors.white),
                             ),
                           ],
                         ),
@@ -118,7 +120,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -168,7 +170,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -218,7 +220,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -266,7 +268,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -309,7 +311,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -352,7 +354,7 @@ class _HomePageState extends State<HomePage> {
                         width: 120,
                         height: 120,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey,
+                          color: Color.fromRGBO(225, 225, 225, 100),
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Center(
@@ -393,7 +395,6 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-          )),
-    );
+          ));
   }
 }
